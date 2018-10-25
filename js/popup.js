d23cc3ec3f3374a3b12023ea23180efd2e06d529
supernova13892 <saranshdhingra92@gmail.com>
2018-10-26 02:48:59 +0530

    Storing of domains in localstorage is complete inside popup

--- js/popup.js
+++ js/popup.js
@@ -2,37 +2,84 @@ let domains = [];
 
 window.onload=function(){
 
-	if(domains.length){
-		let list=document.createElement("ul");
+	//this is supposed to add the current domain to be remembered in localstorage
+	document.getElementById("add_url_btn").addEventListener("click",function(){
+
+		getCurrentUrl(function(url){
+
+			//if the domain is not there, add it!
+			if(domains.indexOf(url)==-1){
+				domains.push(url);
+				chrome.storage.local.set({
+					'selectedDomains': domains,
+				}, function () {
+					showSavedDomains();
+				});
+			}
+
+
+		});
+	});
+
+	document.addEventListener("click",function(e){
+		
+		if(e.target.classList.contains("remove_btn")){
+			let url=e.target.previousSibling.nodeValue;
+
+			//if the domain is already there, remove it!
+			domains.splice(domains.indexOf(url), 1);
+			chrome.storage.local.set({
+				'selectedDomains': domains,
+			}, function () {
+				showSavedDomains();
+			});
+		}
+
+	});
+
+	//initialize the page by fetching currently stored domains
+	fetchDomains(function(){
+		//by now, the 'domains' global contains the remembered domains from localstorage
+		showSavedDomains();
+	});
+	
+}
+
+//draw function? :P
+function showSavedDomains(){
+
+	//clear the div
+	document.getElementById("added_urls").innerHTML='';
+
+	if (domains.length) {
+		let list = document.createElement("ul");
 		for (let i = 0; i < domains.length; i++) {
-			let item=document.createElement("li");
+			let item = document.createElement("li");
+			let removeBtn=document.createElement("span");
+			
+			removeBtn.classList.add("remove_btn");
+			removeBtn.innerHTML = "&#9747;"; //html entity for delete
 			item.appendChild(document.createTextNode(domains[i]));
+			item.appendChild(removeBtn);
 			list.appendChild(item);
 		}
 		document.getElementById("added_urls").appendChild(list);
-	}
-	else{
+	} else {
 		document.getElementById("added_urls").appendChild(document.createTextNode("No URLs added!"));
 	}
-
-	document.addEventListener("click",function(){
-		//this is supposed to add the current domain to be remembered in localstorage
-	});
 
 	checkCurrentUrlAdded();
 }
 
 //checks if the current URL is remembered or not
 function checkCurrentUrlAdded(){
-	console.log("checkcurrenturl");
+	
 	getCurrentUrl(function(currentUrl){
-		console.log("getcurrenturl");
+		
 		if(domains.indexOf(currentUrl)==-1){
-			console.log("index=-1",domains,currentUrl);
 			document.getElementById("add_url_btn").classList.remove("disabled");
 		}
 		else{
-			console.log("index=1");
 			document.getElementById("add_url_btn").classList.add("disabled");
 		}
 		
@@ -51,6 +98,14 @@ function getCurrentUrl(callback){
 }
 
 //fetch all the domains remembered in localstorage
-function fetchDomains(){
+function fetchDomains(callback){
+	chrome.storage.local.get(['selectedDomains'], function(result){
+		
+		if(result.selectedDomains===undefined)
+			domains=[];
+		else
+			domains=result.selectedDomains;
 
+		callback();
+	});
 }
